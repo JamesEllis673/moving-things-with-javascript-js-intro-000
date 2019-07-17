8d9a68b6c23becbfc46b8d8ab8699048f947744f
James Ellis <13jellis@toothillschool.co.uk>
2019-07-17 11:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = "#FF69B4"
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
 document.addEventListener('keydown', function(key) {
   if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
-
+    moveDodgerLeft()
+}
     dodger.style.left = '${left - 1}px'
   }
 })
